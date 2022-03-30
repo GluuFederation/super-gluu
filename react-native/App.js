fe61862d42e7234710e96b0c999c4cef0e5d40df
Harsukhbir <harsukhbirs@gmail.com>
2022-03-30 11:35:27 +0530

    Commit standard
    Signed-off-by: Harsukhbir <harsukhbirs@gmail.com>

--- react-native/App.js
+++ react-native/App.js
@@ -3,6 +3,8 @@ import FlashMessage from "react-native-flash-message";
 import Routes from "./src/Navigation/Routes";
 import { LogBox } from "react-native";
 
+//  Recommit for commit standard (Sign off)
+
 LogBox.ignoreAllLogs();
 export default class App extends Component {
 
